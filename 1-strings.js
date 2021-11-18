b27c94a521c5a18842571868fc2cc69e5fbe68ec
Brndndev <brndndev@outlook.com>
2021-11-18 16:25:05 -0500

    javascript practice

---
+++ 1-strings.js
@@ -0,0 +1,23 @@
+let name
+name = "Brandon"
+// console.log(name)
+
+let favoriteColor
+favoriteColor = "Orange"
+// console.log(favoriteColor)
+
+let favoriteFood
+favoriteFood = "Pizza"
+// console.log(favoriteFood)
+
+let dreamTravelDestination
+dreamTravelDestination = "Hawaii"
+// console.log(dreamTravelDestination)
+
+
+console.log(`Hey! My name is ${name} and my favorite color is ${favoriteColor}! This time next year I hope to be enjoying ${favoriteFood} in ${dreamTravelDestination}`)
+
+let string = "Total Characters"
+let stringLength=string.length
+
+console.log(stringLength)
